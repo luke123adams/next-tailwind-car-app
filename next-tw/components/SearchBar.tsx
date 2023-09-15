273479095534547ecde6c6a99db1bc6c7e8adb82
luke123adams <lxa207@gmail.com>
2023-09-15 13:22:26 +0100

    added autocomplete search bar

---
+++ next-tw/components/SearchBar.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import React, { useState } from 'react'
+import SearchManufacturer from './SearchManufacturer'
+
+export const SearchBar = () => {
+    const [manufacturer, setManufacturer] = useState("")
+    const handleSearch = () => {
+
+    }
+  return (
+    <form className="searchbar" onSubmit=
+    {handleSearch}>
+        <div className="searchbar__item">
+            <SearchManufacturer />
+        </div>
+    </form>
+  )
+}
---
+++ next-tw/components/index.ts
@@ -0,0 +1,17 @@
+import Footer from './Footer';
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import { SearchBar } from './SearchBar';
+import { CustomFilter } from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer
+}
